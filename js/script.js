abbb4ee8827ee959079b379c98dd0812e7324b89
kkyuheak <gyuheak@gmail.com>
2024-09-28 21:15:53 +0900

    리스트 추가하기 기능 구현완료

--- js/script.js
+++ js/script.js
@@ -1,7 +1,35 @@
 // 리스트 추가하기
-const getAddListBtn = document.querySelector(".add_list");
-const getMain = document.querySelector(".main");
+const main = document.querySelector(".main");
 
-getAddListBtn.addEventListener("click", () => {
-  getMain.classList.toggle("open_add_box");
+const getAddListBtn = document.querySelector(".add_list");
+const getBox = document.querySelector(".add_list_box");
+const getBoxItem = getBox.querySelector(".add_list_box_item");
+const getListTitle = document.getElementById("add_list_title");
+
+getAddListBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  getBox.classList.toggle("open_add_box");
+});
+
+getBoxItem.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
+
+window.addEventListener("click", () => {
+  getBox.classList.remove("open_add_box");
+});
+
+getBox.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(getListTitle.value);
+
+  main.insertAdjacentHTML(
+    "beforeend",
+    `<div class="main_box">
+        <h3>${getListTitle.value}</h3>
+        <div class="add_card">+ 카드 추가하기</div>
+      </div>`
+  );
+  getListTitle.value = "";
+  getBox.classList.remove("open_add_box");
 });
